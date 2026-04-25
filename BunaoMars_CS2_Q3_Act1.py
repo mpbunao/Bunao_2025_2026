60b4798c8688b7647f2dc7e30c07b13cb8e541a5
mpbunao <mpbunao@cvisc.pshs.edu.ph>
2026-04-25 21:28:31 +0800

    Add files via upload

---
+++ BunaoMars_CS2_Q3_Act1.py
@@ -0,0 +1,11 @@
+student = {}
+
+student["name"] = input("enter your name:")
+student["age"] = input("enter your age:")
+student["hobbies"] = input("enter your hobbies:")
+print("student record:")
+print("name:", student["name"])
+print("age:", student["age"])
+print("hobbies:", student["hobbies"])
+
+
